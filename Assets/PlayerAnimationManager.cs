8a6bd8cfb6e38c4ac37a50d2821f1bba39b6d993
Dominik Kasprzyk <dominikt.fkasprzyk@gmail.com>
2023-12-28 21:41:49 +0100

    Smooth animation transitions

--- Assets/PlayerAnimationManager.cs
+++ Assets/PlayerAnimationManager.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class PlayerAnimationManager : MonoBehaviour
 {
+    [Min(0)] [SerializeField] private float _fullBlendTime = 0f;
+    
     [Header("References")]
     [SerializeField] private InputManager _inputManager;
+
     private Animator _animator;
+    private Coroutine _verticalCoroutine, _horizontalCoroutine;
+
 
     private void Awake()
     {
@@ -29,17 +34,34 @@ public class PlayerAnimationManager : MonoBehaviour
 
     private void OnMove(Vector2 _movementInput)
     {
-        _animator.SetFloat("Vertical", Mathf.Round(_movementInput.y));
+        _movementInput = new Vector2((float)Math.Round(_movementInput.x), (float)Math.Round(_movementInput.y));
+        if (_verticalCoroutine != null)
+        {
+            StopCoroutine(_verticalCoroutine);
+        }
+        _verticalCoroutine = StartCoroutine(SmoothAnimationTransitionRoutine("Vertical", _movementInput.y));
+
         if (_movementInput.y >= 0)
         {
-            _animator.SetFloat("Horizontal", Mathf.Round(_movementInput.x));
+            if (_horizontalCoroutine != null)
+            {
+                StopCoroutine(_horizontalCoroutine);
+            }
+            _horizontalCoroutine = StartCoroutine(SmoothAnimationTransitionRoutine("Horizontal", _movementInput.x));
+        }
+        else
+        {
+            if (_horizontalCoroutine != null)
+            {
+                StopCoroutine(_horizontalCoroutine);
+            }
+            _horizontalCoroutine = StartCoroutine(SmoothAnimationTransitionRoutine("Horizontal", 0));
         }
     }
 
     private void OnJump()
     {
         _animator.SetBool("isJumping", true);
-        print("jump");
         StartCoroutine(WaitForJumpEnd());
     }
 
@@ -54,7 +76,29 @@ public class PlayerAnimationManager : MonoBehaviour
             currentY = transform.position.y;
 
         } while (previousY != currentY);
-        print("jump end");
         _animator.SetBool("isJumping", false);
+    }
+
+    private IEnumerator SmoothAnimationTransitionRoutine(string variableName, float finalValue)
+    {
+        float startingValue = _animator.GetFloat(variableName);
+        float currentValue = startingValue;
+        float timeBetweenUpdates = .01f;
+        float currentBlendTime = _fullBlendTime * Math.Abs(finalValue - startingValue);
+        float change = currentBlendTime > timeBetweenUpdates ? ((finalValue - startingValue) * timeBetweenUpdates) / currentBlendTime : (finalValue - startingValue);
+
+        while ((change > 0 && currentValue < finalValue) || (change < 0 && currentValue > finalValue))
+        {
+            yield return new WaitForSeconds(timeBetweenUpdates);
+            currentValue += change;
+            if(change > 0)
+            {
+                currentValue = currentValue > finalValue ? finalValue : currentValue;  
+            } else
+            {
+                currentValue = currentValue < finalValue ? finalValue : currentValue;
+            }
+            _animator.SetFloat(variableName, currentValue, .1f, .1f);
+        }
     }
 }
